a89c2b096dd13b7643421374c873284ee80473b0
cjlongoria <57028442+cjlongoria@users.noreply.github.com>
2024-07-13 20:05:46 -0700

    Delete nvim/lua/config directory

--- nvim/lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- avoid mistyping write/quit
-vim.cmd("command WQ wq")
-vim.cmd("command Wq wq")
-vim.cmd("command W w")
-vim.cmd("command Q q")
